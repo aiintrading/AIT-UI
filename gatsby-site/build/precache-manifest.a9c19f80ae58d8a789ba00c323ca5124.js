a64d3f5fb4af9583f04cda1cf67360787d582c0e
ShivshankarPersonal <shivshankar.personal@gmail.com>
2020-07-29 00:15:11 +0530

    other chagnes appeared

---
+++ gatsby-site/build/precache-manifest.a9c19f80ae58d8a789ba00c323ca5124.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e46d2b17743986349e9eab69cab3adcf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c5dffd6b16ac2c69fb3",
+    "url": "/static/js/2.e2edbc52.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.e2edbc52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b43919e3221e43bd320",
+    "url": "/static/js/main.a976dc4b.chunk.js"
+  },
+  {
+    "revision": "8aba14c5515e5362888a",
+    "url": "/static/js/runtime-main.66cf0057.js"
+  }
+]);
